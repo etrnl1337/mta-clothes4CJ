4ca98caf7e459ecaae8c92b3b77ce7a58c92f424
etrnl1337 <97061739+etrnl1337@users.noreply.github.com>
2022-03-12 01:43:36 +0100

    s.lua

--- s.lua
+++ s.lua
@@ -1,1 +1,541 @@
---testx
+    -- slot 0
+    
+    local koszulki = {
+        {"player_torso", "torso"}, 
+        {"vestblack", "vest"}, 
+        {"vest", "vest"}, 
+        {"tshirt2horiz", "tshirt2"},
+        {"tshirtwhite", "tshirt"},
+        {"tshirtilovels", "tshirt"},
+        {"tshirtblunts", "tshirt"},
+        {"shirtbplaid", "shirtb"},
+        {"shirtbcheck", "shirtb"},
+        {"field", "field"},
+        {"tshirterisyell", "tshirt"},
+        {"tshirterisorn", "tshirt"},
+        {"trackytop2eris", "trackytop1"},
+        {"bbjackrim", "bbjack"},
+        {"bballjackrstar", "bbjack"},
+        {"baskballdrib", "baskball"},
+        {"baskballrim", "baskball"},
+        {"sixtyniners", "tshirt"},
+        {"bandits", "baseball"},
+        {"tshirtprored", "tshirt"},
+        {"tshirtproblk", "tshirt"},
+        {"trackytop1pro", "trackytop1"},
+        {"hockeytop", "sweat"},
+        {"bbjersey", "sleevt"},
+        {"shellsuit", "trackytop1"},
+        {"tshirtheatwht", "tshirt"},
+        {"tshirtbobomonk", "tshirt"},
+        {"tshirtbobored", "tshirt"},
+        {"tshirtbase5", "tshirt"},
+        {"tshirtsuburb", "tshirt"},
+        {"hoodyamerc", "hoodya"},
+        {"hoodyabase5", "hoodya"},
+        {"hoodyarockstar", "hoodya"},
+        {"wcoatblue", "wcoat"},
+        {"coach", "coach"},
+        {"sweatrstar", "sweat"},
+        {"hoodyAblue", "hoodyA"},
+        {"hoodyAblack", "hoodyA"},
+        {"hoodyAgreen", "hoodyA"},
+        {"sleevtbrown", "sleevt"},
+        {"shirtablue", "shirta"},
+        {"shirtayellow", "shirta"},
+        {"shirtagrey", "shirta"},
+        {"shirtbgang", "shirtb"},
+        {"tshirtzipcrm", "tshirt"},
+        {"tshirtzipgry", "tshirt"},
+        {"denimfade", "denim"},
+        {"bowling", "hawaii"},
+        {"hoodjackbeige", "hoodjack"},
+        {"baskballloc", "baskball"},
+        {"baskballloc", "baskball"},
+        {"tshirtlocgrey", "tshirt"},
+        {"tshirtmaddgrey", "tshirt"},
+        {"tshirtmaddgrn", "tshirt"},
+        {"suit1grey", "suit1"},
+        {"suit1blk", "suit1"},
+        {"leather", "leather"},
+        {"painter", "painter"},
+        {"hawaiiwht", "hawaii"},
+        {"hawaiired", "hawaii"},
+        {"sportjack", "trackytop1"},
+        {"suit1red", "suit1"},
+        {"suit1blue", "suit1"},
+        {"suit1yellow", "suit1"},
+        {"suit2grn", "suit2"},
+        {"tuxedo", "suit2"},
+        {"suit1gang", "suit1"},
+        {"letter", "sleevt"}
+        }
+        
+        -- slot 1
+        
+        local fryzury = {
+        {"player_face", "head"},
+        {"hairblond", "head"},
+        {"hairred", "head"},
+        {"hairblue", "head"},
+        {"hairgreen", "head"},
+        {"hairpink", "head"},
+        {"bald", "head"},
+        {"baldbeard", "head"},
+        {"baldtash", "head"},
+        {"baldgoatee", "head"},
+        {"highfade", "head"},
+        {"highafro", "highafro"},
+        {"wedge", "wedge"},
+        {"slope", "slope"},
+        {"jhericurl", "jheri"},
+        {"cornrows", "cornrows"},
+        {"cornrowsb", "cornrows"},
+        {"tramline", "tramline"},
+        {"groovecut", "groovecut"},
+        {"mohawk", "mohawk"},
+        {"mohawkblond", "mohawk"},
+        {"mohawkpink", "mohawk"},
+        {"mohawkbeard", "mohawk"},
+        {"afro", "afro"},
+        {"afrotash", "afro"},
+        {"afrobeard", "afro"},
+        {"afroblond", "afro"},
+        {"flattop", "flattop"},
+        {"elvishair", "elvishair"},
+        {"beard", "head"},
+        {"tash", "head"},
+        {"goatee", "head"},
+        {"afrogoatee", "head"}
+        }
+        
+        -- slot 2
+        
+        local spodnie = {
+        {"player_legs", "legs"},
+        {"worktrcamogrn", "worktr"},
+        {"worktrcamogry", "worktr"},
+        {"worktrgrey", "worktr"},
+        {"worktrkhaki", "worktr"},
+        {"tracktr", "tracktr"},
+        {"tracktreris", "tracktr"},
+        {"jeansdenim", "jeans"},
+        {"legsblack", "legs"},
+        {"legsheart", "legs"},
+        {"biegetr", "chinosb"},
+        {"tracktrpro", "tracktr"},
+        {"tracktrwhstr", "tracktr"},
+        {"tracktrblue", "tracktr"},
+        {"tracktrgang", "tracktr"},
+        {"bbshortwht", "boxingshort"},
+        {"boxshort", "boxingshort"},
+        {"bbshortred", "boxingshort"}, 
+        {"shellsuittr", "tracktr"}, 
+        {"shortsgrey", "shorts"}, 
+        {"shortskhaki", "shorts"}, 
+        {"chongergrey", "chonger"}, 
+        {"chongergang", "chonger"}, 
+        {"chongerred", "chonger"}, 
+        {"chongerblue", "chonger"}, 
+        {"shortsgang", "shorts"}, 
+        {"denimsgang", "jeans"}, 
+        {"denimsred", "jeans"}, 
+        {"chinosbiege", "chinosb"}, 
+        {"chinoskhaki", "chinosb"}, 
+        {"cutoffchinos", "shorts"}, 
+        {"cutoffchinosblue", "shorts"}, 
+        {"chinosblack", "chinosb"}, 
+        {"chinosblue", "chinosb"}, 
+        {"leathertr", "leathertr"}, 
+        {"leathertrchaps", "leathertr"}, 
+        {"suit1trgrey", "suit1tr"}, 
+        {"suit1trblk", "suit1tr"}, 
+        {"cutoffdenims", "shorts"}, 
+        {"suit1trred", "suit1tr"}, 
+        {"suit1trblue", "suit1tr"}, 
+        {"suit1tryellow", "suit1tr"}, 
+        {"suit1trgreen", "suit1tr"}, 
+        {"suit1trblk2", "suit1tr"}, 
+        {"suit1trgang", "suit1tr"}
+        }
+        
+        -- slot 3
+        
+        local buty = {
+        {"foot", "feet"},
+        {"cowboyboot2", "biker"},
+        {"bask2semi", "bask1"},
+        {"bask1eris", "bask1"},
+        {"sneakerbincgang", "sneaker"},
+        {"sneakerbincblu", "sneaker"},
+        {"sneakerbincblk", "sneaker"},
+        {"sandal", "flipflop"},
+        {"sandalsock", "flipflop"},
+        {"flipflop", "flipflop"},
+        {"hitop", "bask1"},
+        {"convproblk", "conv"},
+        {"convproblu", "conv"},
+        {"convprogrn", "conv"},
+        {"sneakerprored", "sneaker"},
+        {"sneakerproblu", "sneaker"},
+        {"sneakerprowht", "sneaker"},
+        {"bask1prowht", "bask1"},
+        {"bask1problk", "bask1"},
+        {"boxingshoe", "biker"},
+        {"convheatblk", "conv"},
+        {"convheatred", "conv"},
+        {"convheatorn", "conv"},
+        {"sneakerheatwht", "sneaker"},
+        {"sneakerheatgry", "sneaker"},
+        {"sneakerheatblk", "sneaker"},
+        {"bask2heatwht", "bask1"},
+        {"bask2heatband", "bask1"},
+        {"timbergrey", "bask1"},
+        {"timberred", "bask1"},
+        {"timberfawn", "bask1"},
+        {"timberhike", "bask1"},
+        {"cowboyboot", "biker"},
+        {"biker", "biker"},
+        {"snakeskin", "biker"},
+        {"shoedressblk", "shoe"},
+        {"shoedressbrn", "shoe"},
+        {"shoespatz", "shoe"}
+        }
+        
+        -- slot 4
+        
+        local lua = {
+        
+            {"4WEED", "4weed"}, 
+            {"4RIP", "4rip"}, 
+            {"4SPIDER", "4spider"}
+        }
+        
+        -- slot 5
+        
+        local lla = {
+        
+            {"5GUN", "5gun"}, 
+            {"5CROSS", "5cross"}, 
+            {"5CROSS2", "5cross2"}, 
+            {"5CROSS3", "5cross3"}
+            
+        }
+        
+        -- slot 6
+        
+        local rua = {
+            {"6AZTEC", "6aztec"},
+            {"6CROWN", "6crown"},
+            {"6CLOWN", "6clown"},
+            {"6AFRICA", "6africa"}
+        
+        }
+        
+        -- slot 7
+        
+        local rla = {
+        
+            {"7CROSS", "7cross"},
+            {"7CROSS2", "7cross2"},
+            {"7CROSS3", "7cross3"},
+            {"7MARY", "7mary"}
+        
+        }
+        
+        -- slot 8
+        
+        local back = {
+        
+            {"8SA", "8sa"},
+            {"8SA2", "8sa2"},
+            {"8SA3", "8sa3"},
+            {"8WESTSD", "8westside"},
+            {"8SANTOS", "8santos"},
+            {"8POKER", "8poker"},
+            {"8GUN", "8gun"}
+        
+        }
+        
+        -- slot 9
+        
+        local lc = {
+        
+            {"9CROWN", "9crown"},
+            {"9GUN", "9gun"},
+            {"9GUN2", "9gun2"},
+            {"9HOMBY", "9homeboy"},
+            {"9BULLT", "9bullet"},
+            {"9RASTA", "9rasta"}
+        
+        }
+        
+        -- slot 10
+        
+        local rc = {
+        
+            {"10LS", "10ls"},
+            {"10LS2", "10ls2"},
+            {"10LS3", "10ls3"},
+            {"10LS4", "10ls4"},
+            {"10ls5", "10ls5"},
+            {"10OG", "10og"},
+            {"10WEED", "10weed"}
+        
+        }
+        
+        -- slot 11
+        local stomach = {
+        
+            {"11GROVE", "11grove"},
+            {"11GROV2", "11grove2"},
+            {"11GROV3", "11grove3"},
+            {"11DICE", "11dice"},
+            {"11DICE2", "11dice2"},
+            {"11JAIL", "11jail"},
+            {"11GGIFT", "11godsgift"}
+        
+        }
+        
+        -- slot 12
+        
+        local lb = {
+        
+            {"12ANGEL", "12angels"},
+            {"12MAYBR", "12mayabird"},
+            {"12DAGER", "12dagger"},
+            {"12BNDIT", "12bandit"},
+            {"12CROSS", "12cross7"},
+            {"12MYFAC", "12mayafce"}
+        
+        }
+        
+        -- slot 13
+        
+        local naszyjnik = {
+            {"dogtag", "neck"},
+            {"neckafrica", "neck"},
+            {"stopwatch", "neck"},
+            {"necksaints", "neck"},
+            {"neckhash", "neck"},
+            {"necksilver", "neck2"},
+            {"neckgold", "neck2"},
+            {"neckropes", "neck2"},
+            {"neckropeg", "neck2"},
+            {"neckls", "neck"},
+            {"neckdollar", "neck"},
+            {"neckcross", "neck"}
+        
+        }
+        
+        -- slot 14
+        
+        local zegarek = {
+            {"watchpink", "watch"},
+            {"watchyellow", "watch"},
+            {"watchpro", "watch"},
+            {"watchpro2", "watch"},
+            {"watchsub1", "watch"},
+            {"watchsub2", "watch"},
+            {"watchzip1", "watch"},
+            {"watchzip2", "watch"},
+            {"watchgno", "watch"},
+            {"watchgno2", "watch"},
+            {"watchcro", "watch"},
+            {"watchcro2", "watch"}
+        }
+        
+        -- slot 15
+        
+        local okulary = {
+            {"groucho", "grouchos"},
+            {"zorro", "zorromask"},
+            {"eyepatch", "eyepatch"},
+            {"glasses01", "glasses01"},
+            {"glasses04", "glasses04"},
+            {"bandred3", "bandmask"},
+            {"bandblue3", "bandmask"},
+            {"bandgang3", "bandmask"},
+            {"bandblack3", "bandmask"},
+            {"glasses01dark", "glasses01"},
+            {"glasses04dark", "glasses04"},
+            {"glasses03", "glasses03"},
+            {"glasses03red", "glasses03"},
+            {"glasses03blue", "glasses03"},
+            {"glasses03dark", "glasses03"},
+            {"glasses05dark", "glasses03"},
+            {"glasses05", "glasses03"}
+        
+        }
+        
+        -- slot 16
+        
+        local czapki = {
+            {"bandred", "bandana"},
+            {"bandblue", "bandana"},
+            {"bandgang", "bandana"},
+            {"bandblack", "bandana"},
+            {"bandred2", "bandknots"},
+            {"bandblue2", "bandknots"},
+            {"bandblack2", "bandknots"},
+            {"bandgang2", "bandknots"},
+            {"capknitgrn", "capknit"},
+            {"captruck", "captruck"},
+            {"cowboy", "cowboy"},
+            {"hattiger", "cowboy"},
+            {"helmet", "helmet"},
+            {"moto", "moto"},
+            {"boxingcap", "boxingcap"},
+            {"hockey", "hockeymask"},
+            {"capgang", "cap"},
+            {"capgangback", "capback"},
+            {"capgangside", "capside"},
+            {"capgangover", "capovereye"},
+            {"capgangup", "caprimup"},
+            {"bikerhelmet", "bikerhelmet"},
+            {"capred", "cap"},
+            {"capredback", "capback"},
+            {"capredside", "capside"},
+            {"capredover", "capovereye"},
+            {"capredup", "caprimup"},
+            {"capblue", "cap"},
+            {"capblueback", "capback"},
+            {"capblueside", "capside"},
+            {"capblueover", "capovereye"},
+            {"capblueup", "caprimup"},
+            {"skullyblk", "skullycap"},
+            {"skullygrn", "skullycap"},
+            {"hatmancblk", "hatmanc"},
+            {"hatmancplaid", "hatmanc"},
+            {"capzip", "cap"},
+            {"capzipback", "capback"},
+            {"capzipside", "capside"},
+            {"capzipover", "capovereye"},
+            {"capzipup", "caprimup"},
+            {"beretred", "beret"},
+            {"beretblk", "beret"},
+            {"capblk", "cap"},
+            {"capblkback", "capback"},
+            {"capblkside", "capside"},
+            {"capblkover", "capovereye"},
+            {"capblkup", "caprimup"},
+            {"trilbydrk", "trilby"},
+            {"trilbylght", "trilby"},
+            {"bowler", "bowler"},
+            {"bowlerred", "bowler"},
+            {"bowlerblue", "bowler"},
+            {"bowleryellow", "bowler"},
+            {"boater", "boater"},
+            {"bowlergang", "bowler"},
+            {"boaterblk", "boater"}
+        
+        }
+
+function setCJClothes(plr,cmd)
+
+        if getElementModel(plr) ~= 0 then outputChatBox("#FFCC00[CLASSIC-RPG] #FFFFFFPosiadasz niewłaściwego skina!", plr, 255, 255, 255, true) return end
+    
+        if isPedInVehicle(plr) then outputChatBox("#FFCC00[CLASSIC-RPG] #FFFFFFNajpierw wysiądź z pojazdu!", plr, 255, 255, 255, true) return end
+    
+        local randTshirt = math.random(1, 68)
+    
+        local randHead = math.random(1, 33) 
+    
+        local randShorts= math.random(1, 45)
+    
+        local randBoots = math.random(1, 38)
+    
+        local randLua = math.random(1, 3)
+    
+        local randLla = math.random(1, 4)
+    
+        local randRua = math.random(1, 4)
+    
+        local randRla = math.random(1, 4)
+    
+        local randBack = math.random(1, 7)
+    
+        local randLc = math.random(1, 6)
+    
+        local randRc = math.random(1, 7)
+    
+        local randStomach = math.random(1, 7)
+    
+        local randLb = math.random(1, 6)
+    
+        local randNaszyjnik = math.random(1, 12)
+    
+        local randZegarek = math.random(1, 12)
+    
+            addPedClothes(plr, koszulki[randTshirt][1], koszulki[randTshirt][2], 0)
+    
+            addPedClothes(plr,fryzury[randHead][1], fryzury[randHead][2], 1)
+    
+            addPedClothes(plr, spodnie[randShorts][1], spodnie[randShorts][2], 2) 
+    
+            addPedClothes(plr, buty[randBoots][1], buty[randBoots][2], 3)
+    
+            addPedClothes(plr, lua[randLua][1],lua[randLua][2], 4)
+    
+            addPedClothes(plr, lla[randLla][1],lla[randLla][2], 5)
+    
+            addPedClothes(plr, rua[randRua][1],rua[randRua][2], 6)
+    
+            addPedClothes(plr, rla[randRla][1],rla[randRla][2], 7)
+    
+            addPedClothes(plr, back[randBack][1],back[randBack][2], 8)
+    
+            addPedClothes(plr, lc[randLc][1],lc[randLc][2], 9)
+    
+            addPedClothes(plr, rc[randRc][1],rc[randRc][2], 10)
+    
+            addPedClothes(plr, stomach[randStomach][1],stomach[randStomach][2], 11)
+    
+            addPedClothes(plr, lb[randLb][1],lb[randLb][2], 12)
+    
+            addPedClothes(plr, naszyjnik[randNaszyjnik][1],naszyjnik[randNaszyjnik][2], 13)
+    
+            addPedClothes(plr, zegarek[randZegarek][1],zegarek[randZegarek][2], 14)
+    
+            local oksy = math.random(1,10)
+            
+            -- 20% na to że będą okulary
+
+            if oksy > 8 then
+    
+                local randOkulary = math.random(1, 17)
+    
+                addPedClothes(plr, okulary[randOkulary][1],okulary[randOkulary][2], 15)
+
+            else
+            
+                local txt, mdl = getPedClothes (plr, 15)
+    
+                if txt then
+                removePedClothes ( plr, 15, txt, mdl )
+                end
+    
+            end
+    
+            local czapa = math.random(1,10)
+
+            -- 20% na to że będzie czapka
+
+            if czapa > 8 then
+    
+                local randCzapki = math.random(1, 57)
+    
+                addPedClothes(plr, czapki[randCzapki][1],czapki[randCzapki][2], 16)
+            
+            else
+            
+                local texture, model = getPedClothes ( plr, 16 )
+    
+                if texture then
+                removePedClothes ( plr, 16, texture, model )
+                end    
+    
+            end
+    
+        end 
+addCommandHandler("cj",setCJClothes, false, false)
